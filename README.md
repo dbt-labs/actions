3359539bb42c18ce4764f27025e25785be236f40
Kyle Wigley <kyle@fishtownanalytics.com>
2022-01-31 23:26:31 -0500

    Add parse semver action w/ tests (#1)

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# actions
+# GitHub Actions for Maintaining dbt
+
+A set of [GitHub Actions](https://github.com/features/actions) for automating common tasks related to developing, maintaining, and releasing dbt-core and database adapter plugins. See individual actions for more info and instructions on how to use.
+
+- [Parse Semver Action](parse-semver)
+
+### Development
+
+- Each Action will have instructions for development.
+- It is recommended to use [act](https://github.com/nektos/act) for testing locally where possible.
+- Actions have a cooresponding workflow for automated testing.
+- Here is [documentation](https://docs.github.com/en/actions/creating-actions) for creating custom actions.
---
+++ parse-semver/main.py
@@ -0,0 +1,35 @@
+import os
+from packaging.version import parse, Version
+
+
+def main():
+    input_version = os.environ["INPUT_VERSION"]
+    parsed_version = parse(input_version)
+    assert parsed_version.release, f"Not a valid version: {input_version}"
+    assert isinstance(parsed_version, Version)
+
+    # ('rc', 2) -> pre_release_type = rc, pre_release_version = 2
+    pre_release_type, pre_release_version = (
+        parsed_version.pre
+        if parsed_version.pre else ('', '')
+    )
+    assert isinstance(pre_release_type, str),\
+        f"Not a valid pre-release type (ex: rc, b): {pre_release_type}"
+
+    pre_release = pre_release_type + str(pre_release_version)
+    is_pre_release = parsed_version.pre is not None
+    is_pre_release_truthy = 1 if is_pre_release else 0
+
+    print(f"::set-output name=version::{parsed_version.public}")
+    print(f"::set-output name=base-version::{parsed_version.base_version}")
+    print(f"::set-output name=major::{parsed_version.major}")
+    print(f"::set-output name=minor::{parsed_version.minor}")
+    print(f"::set-output name=patch::{parsed_version.micro}")
+    print(f"::set-output name=pre-release-type::{pre_release_type}")
+    print(f"::set-output name=pre-release-version::{pre_release_version}")
+    print(f"::set-output name=pre-release::{pre_release}")
+    print(f"::set-output name=is-pre-release::{is_pre_release_truthy}")
+
+
+if __name__ == "__main__":
+    main()
