9c18c7420c1e317e212daa9b78d41f81ef0e792e
Mike Alfare <mike.alfare@dbtlabs.com>
2024-04-22 20:39:11 -0400

    standardize setup-environment action to hatch/environment/create

---
+++ hatch/environment/create/install_dependencies.sh
@@ -0,0 +1,7 @@
+python -m pip install --upgrade \
+    hatch \
+    pip
+
+export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin
+brew tap miniscruff/changie https://github.com/miniscruff/changie
+brew install changie
