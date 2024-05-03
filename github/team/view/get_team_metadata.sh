4072a8b98d256e79d2623c620e19582d58eda327
Mike Alfare <mike.alfare@dbtlabs.com>
2024-05-03 19:33:56 -0400

    add view github team action

---
+++ github/team/view/get_team_metadata.sh
@@ -0,0 +1,9 @@
+OUTPUT_FILE=output_$GITHUB_RUN_ID.json
+
+gh api $ENDPOINT -H "$HEADER" > $OUTPUT_FILE
+
+team_list=$(jq -r '.[].login' $OUTPUT_FILE)
+team_list_single=$(echo $team_list | tr '\n' ' ')
+echo "members=$team_list_single" >> $GITHUB_OUTPUT
+
+rm $OUTPUT_FILE
